f0ed51e75ee9e666da93ac507e06e5140b7e7b1e
Thomas POULARD <pro.thomaspoulard@gmail.com>
2025-04-09 18:59:04 +0200

    refactor: create all api routes for profile

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfilController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/profiles', [ProfilController::class, 'index']);
+Route::prefix('v1')->group(function () {
+    Route::group(['prefix' => 'profiles', 'as' => 'profiles.'], function () {
+        Route::get('/', [ProfilController::class, 'index'])->name('index');
+        Route::post('/create', [ProfilController::class, 'store'])->name('store');
+        Route::put('/{id}', [ProfilController::class, 'update'])->name('update');
+        Route::delete('/{id}', [ProfilController::class, 'destroy'])->name('destroy');
+    });
+});
