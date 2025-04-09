b54cd076cd4938eeacaf95b64befa1212badca41
Thomas POULARD <pro.thomaspoulard@gmail.com>
2025-04-10 01:18:09 +0200

    refactor: remove unused import

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Profil;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
 class ProfilController extends Controller
