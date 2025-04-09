cc5f0fc3791486af964a7f90395483175420e9d5
Thomas POULARD <pro.thomaspoulard@gmail.com>
2025-04-09 20:19:01 +0200

    feat: store profile api with formRequest validation

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -4,28 +4,67 @@ namespace App\Http\Controllers;
 
 use App\Models\Profil;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class ProfilController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $profiles = Profil::whereHas('statut', function($query) {
+        $profiles = Profil::whereHas('statut', function ($query) {
             $query->where('nom', 'like', 'actif');
         })->select('nom', 'prenom', 'image', 'created_at', 'updated_at')
-        ->get();
+            ->get();
+
+        if ($profiles->isEmpty()) {
+            return response()->json(['message' => 'Il n\'y a aucun profil actif actuellement.'], 404);
+        }
+
+        //TODO: Handle auth status to see the "statut" field
 
         return response()->json($profiles);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validated = $request->validate([
+                'nom' => 'required|string|max:255',
+                'prenom' => 'required|string|max:255',
+                'image' => [
+                    'required',
+                    'image',
+                    'mimes:jpg,png,jpeg,gif,svg',
+                    'max:2048'
+                ],
+                'statut_id' => [
+                    'required',
+                    'integer',
+                    'exists:statuts,id'
+                ],
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'errors' => $e->errors(),
+                'message' => 'Validation failed',
+            ], 422);
+        }
+
+        $profil = Profil::create($validated);
+
+        //TODO: Handle local image storage or base64 here
+
+        return response()->json($profil);
     }
 
     /**
