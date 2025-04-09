a9cff7b14967b688f08c93e5e5ef0f71e6d0283b
Thomas POULARD <pro.thomaspoulard@gmail.com>
2025-04-09 15:51:50 +0200

    feat: generate Administrateur migration, model, controller, factory

---
+++ app/Models/Administrateur.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Administrateur extends Model
+{
+    /** @use HasFactory<\Database\Factories\AdministrateurFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+}
