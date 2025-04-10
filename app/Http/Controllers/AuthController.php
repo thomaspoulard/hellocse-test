5dddc7adf3580f5da20d2ceb5c46605cab53fe0d
Thomas POULARD <pro.thomaspoulard@gmail.com>
2025-04-10 12:31:22 +0200

    feat: admin register api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,50 @@
 
 namespace App\Http\Controllers;
 
-abstract class AuthController
+use App\Models\Administrateur;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
 {
-    //
+    /**
+     * Register an admin into the database.
+     *
+     * @param \Illuminate\Http\Request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $admin = new Administrateur;
+
+        try {
+            $validated = $request->validate([
+                'email' => 'required|email|unique:administrateurs',
+                'password' => 'required|min:6',
+            ]);
+
+        } catch (ValidationException $e) {
+            return response()->json([
+                'errors' => $e->errors(),
+                'message' => 'La validation des champs a échoué.',
+            ], 422);
+        }
+
+
+        $admin = Administrateur::create([
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $admin->createToken('authToken')->plainTextToken; //TODO: replace string by value of createNewToken qui a un admin en param method from TokenService;
+
+        return response()->json([
+            'email' => $admin->email,
+            'remember_token' => $token
+        ], 200);
+    }
+
+    public function login() {}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+    Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+        Route::post('/register', [AuthController::class, 'register'])->name('register');
+        Route::post('/login', [AuthController::class, 'login'])->name('login');
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
+
     Route::group(['prefix' => 'profiles', 'as' => 'profiles.'], function () {
         Route::get('/', [ProfilController::class, 'index'])->name('index');
         Route::post('/create', [ProfilController::class, 'store'])->name('store');
