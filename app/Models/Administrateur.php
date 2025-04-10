64cbdf35f3e5b30a1bf6e7dd16ede44b427e3023
Thomas POULARD <pro.thomaspoulard@gmail.com>
2025-04-10 10:47:22 +0200

    feat: fill Administrateur entity and migration attributes

--- app/Models/Administrateur.php
+++ app/Models/Administrateur.php
@@ -2,9 +2,43 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Administrateur extends Model
+class Administrateur extends Authenticatable
 {
-    //
+    use HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
 }
